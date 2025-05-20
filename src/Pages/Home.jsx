90b1d33a3d24aeb2c2a93b8784f96a65a45585fa
Ambs04 <amberleejones2007@gmail.com>
2025-05-20 22:25:59 +0200

    simple fetch from api and display the data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,31 @@
+import { useState, useEffect } from "react";
+
 export default function Home() {
-  return <h1>HOME PAGE</h1>;
+  const [podState, setPodState] = useState([]);
+
+  useEffect(() => {
+    fetch("https://podcast-api.netlify.app/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPodState(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {podState.map((pods) => (
+        <div key={pods.id}>
+          <img src={pods.image} />
+          <div>
+            <h3>{pods.title}</h3>
+            <p>{pods.description}</p>
+          </div>
+          <div>
+            <span>{pods.genres}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
