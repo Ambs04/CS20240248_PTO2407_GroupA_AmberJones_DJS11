cd4237ca1587b854abbdf15c4bbc18434d43117e
Ambs04 <amberleejones2007@gmail.com>
2025-05-22 01:55:32 +0200

    fix styling

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <div>
+        <div>
+          <p>© PodStar</p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="header">
         <div className="logo-container">
           <img src="logo.png" className="logo" />
-          <h2 className="website-name">PODSTAR</h2>
+          <h2 className="website-name">PodStar</h2>
         </div>
         <div className="search-container">
           <input type="text" placeholder="search" className="search-bar" />
