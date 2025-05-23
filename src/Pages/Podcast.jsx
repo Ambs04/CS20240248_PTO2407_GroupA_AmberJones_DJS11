24e8d59cc6dbe24bb8e57347981e4b7f8349f188
Ambs04 <amberleejones2007@gmail.com>
2025-05-23 16:26:49 +0200

    create podcast  page with simple api fetch according to podcast's id

--- src/Pages/Podcast.jsx
+++ src/Pages/Podcast.jsx
@@ -1,6 +1,35 @@
-//import {useState, useEffect} from 'react-dom'
-//import
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Podcast() {
-  return <h1>podcast Page</h1>;
+  const { id } = useParams();
+
+  const [podcast, setPodcast] = useState([]);
+
+  useEffect(() => {
+    fetch("https://podcast-api.netlify.app/")
+      .then((res) => res.json())
+      .then((data) => {
+        const selectedId = data.find((podcast) => podcast.id === id);
+        setPodcast(selectedId);
+      });
+  }, [id]);
+
+  //const updatedDate = ;
+
+  return (
+    <>
+      <div>
+        <div>
+          <img src={podcast.image} />
+        </div>
+        <h2>{podcast.title}</h2>
+        <p>{podcast.description}</p>
+
+        {podcast.updated && (
+          <p>{new Date(podcast.updated).toLocaleDateString("en-ZA")}</p>
+        )}
+      </div>
+    </>
+  );
 }
