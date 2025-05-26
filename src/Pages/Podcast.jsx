e00823572eeffb5d361503e10e5a841f8ef84e90
Ambs04 <amberleejones2007@gmail.com>
2025-05-26 14:34:15 +0200

    set state for the selected season to display relevent image when clicked

--- src/Pages/Podcast.jsx
+++ src/Pages/Podcast.jsx
@@ -1,15 +1,13 @@
 import "./podcast.css";
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-//import Player from "../Components/Player";
-//import { AudioPlayer } from "react-audio-play";
-//import Player from "../Components/Player";
 
 export default function Podcast({ onPlay }) {
   const { id } = useParams();
 
   const [podcast, setPodcast] = useState([]);
   const [seasons, setSeasons] = useState([]);
+  const [selectedSeason, setSelectedSeason] = useState([]);
 
   useEffect(() => {
     fetch("https://podcast-api.netlify.app/")
@@ -24,8 +22,8 @@ export default function Podcast({ onPlay }) {
     fetch(`https://podcast-api.netlify.app/id/${id}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setSeasons(data.seasons);
+        selectedSeason(data.seasons);
       });
   }, [id]);
 
@@ -34,7 +32,7 @@ export default function Podcast({ onPlay }) {
       {/* <div className="container"> */}
       <div className="pod-container">
         <div className="image-container">
-          <img src={podcast.image} className="image" />
+          <img src={selectedSeason?.image || podcast.image} className="image" />
         </div>
         <div className="info">
           <h2 className="title">{podcast.title}</h2>
@@ -59,9 +57,13 @@ export default function Podcast({ onPlay }) {
         <div className="show">
           {seasons.map((season) => (
             <>
-              <div key={season.id} className="seasons">
-                <h2>Season {season.season}:</h2>
-              </div>
+              <button
+                key={season.id}
+                className="seasons"
+                onClick={() => setSelectedSeason(season)}
+              >
+                <>Season {season.season}:</>
+              </button>
               {season.episodes.map((episode) => (
                 <div key={episode.id} className="episodes">
                   <p>Episode{episode.episode}</p>
