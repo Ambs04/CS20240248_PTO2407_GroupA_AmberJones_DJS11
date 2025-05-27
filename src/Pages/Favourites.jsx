9b3c705deeff3f22b2ccc4a451a5deac0a30c64f
Ambs04 <amberleejones2007@gmail.com>
2025-05-27 18:41:47 +0200

    set state to the favourites list in localStorage

--- src/Pages/Favourites.jsx
+++ src/Pages/Favourites.jsx
@@ -1,3 +1,14 @@
+import { useState, useEffect } from "react";
+
 export default function Favourites() {
+  //set state for favourites
+  const [favourites, setFavourites] = useState([]);
+
+  //set state to the favourites list that is fetched from localStorage
+  useEffect(() => {
+    const storage = JSON.parse(localStorage.getItem("favourites"));
+    setFavourites(storage);
+  }, []);
+
   return <h1>favourites</h1>;
 }
