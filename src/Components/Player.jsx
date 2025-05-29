b7dd53d3c8f8b5f4585d365678d58d06d8010854
Ambs04 <amberleejones2007@gmail.com>
2025-05-29 12:18:00 +0200

    style player component

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,5 +1,20 @@
 import { AudioPlayer } from "react-audio-play";
+import "./player.css";
 
 export default function Player({ src }) {
-  return <AudioPlayer src={src} loop autoPlay controls />;
+  return (
+    <div className="audio-wrapper">
+      <AudioPlayer
+        src={src}
+        loop
+        autoPlay
+        hasKeyBindings
+        className="player"
+        sliderColor="#ffffff"
+        backgroundColor="rgb(22, 22, 72)"
+        color="#ffffff"
+        style={{ borderRadius: "10px", height: "60px" }}
+      />
+    </div>
+  );
 }
